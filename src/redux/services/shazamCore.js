f1b843580dcb64fca0d6e1e7cc47f50e68a5bce9
Ioannis Kotsonis <JohnKotsGR@gmail.com>
2025-01-20 00:19:17 +0200

    Last commit

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,18 +1,32 @@
-import axios from "axios";
-const API_KEY = import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY;
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const options = {
-  method: 'GET',
-  url: 'https://shazam-core.p.rapidapi.com/v1/charts/world',
-  params: {country_code: 'DZ'},
-  headers: {
-    'x-rapidapi-key': {API_KEY},
-    'x-rapidapi-host': 'shazam-core.p.rapidapi.com'
-  }
-};
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/',
+    prepareHeaders: (headers) => {
+      headers.set('X-RapidAPI-Key', import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY);
 
-axios.request(options).then(function (response) {
-  console.log(response.data);
-}).catch(function (error) {
-  console.error(error);
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => 'v1/charts/world?country_code=GR' }),
+    getSongsByGenre: builder.query({ query: (genre) => `v1/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `v1/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `v1/tracks/related?track_id=${songid}` }),
+  }),
 });
+
+export const {
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
